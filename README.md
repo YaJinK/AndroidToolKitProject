9e41da23c5739136c90c014095fe59e2f1ead15c
kagawa <kagawa@91fifa.com>
2021-12-28 14:29:12 +0800

    工具库更新

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# AndroidToolKitProject
-Android工具集
+Android 工具集
+1、隐私政策
+    隐私政策提前到第一个Activity，防止引擎或者sdk提前获取用户信息
+2、工具类
+    提供反射获取R文件等操作
--- ToolKitProject/utility/src/main/java/com/magata/utility/Utility.java
+++ ToolKitProject/utility/src/main/java/com/magata/utility/Utility.java
@@ -1,5 +1,8 @@
 package com.magata.utility;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
 
 import java.lang.reflect.Field;
@@ -9,6 +12,9 @@ public class Utility {
 
     private static String TAG = "Log Utility";
 
+    ///*
+    // 反射获取R文件内容
+    // *///
     public static int getRContent(String packageName, String className, String resouceName) {
         try {
             Class<?> resource = Class.forName(packageName + ".R");
@@ -34,5 +40,43 @@ public class Utility {
             e.printStackTrace();
         }
         return 0;
+    }
+
+    /****************
+
+     * @param url Activity配置的url
+
+     * @return 返回true表示呼起界面成功，返回false表示呼起失败
+
+     ******************/
+    public boolean startActivity(Context context, String url) {
+        Intent intent = new Intent();
+        intent.setData(Uri.parse(url));
+        try {
+            context.startActivity(intent);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    /****************
+
+     * @param url Activity配置的url
+
+     * @param mimeType mimeType
+
+     * @return 返回true表示呼起界面成功，返回false表示呼起失败
+
+     ******************/
+    public boolean startActivity(Context context, String url, String mimeType) {
+        Intent intent = new Intent();
+        intent.setDataAndType(Uri.parse(url), mimeType);
+        try {
+            context.startActivity(intent);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
